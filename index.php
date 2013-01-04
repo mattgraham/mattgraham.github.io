c0ab79d738beed38d51c0e1529c288978915b2db
Matt Graham <graham@github.com>
2013-01-03 20:53:14 -0500

    stopping point

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+	<script type="text/javascript" src="//use.typekit.net/wpw2zdl.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	<link rel="stylesheet" href="stylesheets/base.css" />
+</head>
+	<body>
+		<header>
+			<div id="logo" class="wrapper">
+				<img src="http://0.gravatar.com/avatar/25d643886852ad60d0efe0b3190a53dd?size=240" class="gravatar">
+				<span class="name">Matt Graham</span>
+				<nav>
+					<li>Home</li>
+					<li>Articles</li>
+					<li>Talks</li>
+					<li>Twitter</li>
+					<li>GitHub</li>
+				<nav>
+			</div>
+		</header>
+		<div class="wrapper">
+			<div id="featured">
+				<img src="/images/gauges/featured.png">
+			</div>
+			<div id="content">
+				<div id="meta">
+					<span class="date">Thurs., July 18, 2012</span>
+					<span>in <a href="#">Shipments</a></span>
+				</div>
+				<div id="article">
+					<h1>Shipping GitHub for Android &amp releasing the code to the wild.</h1>
+					<p>Today we've shipped GitHub for Android and I've learned much in the process. The app is free to download on Google Play and you can also browse the code from the newly open sourced repository. I like to think of this as, Incredible beginings.</p>
+					<p>This first release of the app includes support for working with Issues and Gists, integrated news feed, quick dashboard access for all your issues and discussions for your issues. We've also added the ability to bookmark any repo's issue list with filters for labels, milestones, and assignees. If you missed it above, we've open sourced the app. We know we're a little crazy but we love giving things away to the development word.</p>
+					<p>I am an Apple fanboy and use the iPhone; however, it was my first goal to make the Android app as sexy and native to the OS as possible. I spent lots of time using, downloading, and reading what makes the OS appealing to their user base. I have several droid phones and we've used the tablets to create the best first release we could have without wasting time shipping it. We know that the app is in its initial release, but we think we did a good job. We're looking forward to adding pull requests, diffs, and much more, but wanted to get a solid app to the market so you could enjoy.</p>
+					<p>I had a blast working along side the lead developer Kevin Sawicki and Tony Jaramillo who designed the amazing new Androidtocat.</p>
+					<p>Have thoughts? Tweet them to me <a href="#">@michigangraham</a> or just follow me because we tweet amazing things happening all the time.</p>
+				</div>
+			</div>
+			<div id="controls">
+				<div id="next">Next > <a href="harmony.php">Harmony</a></div>
+			</div>
+	</body>
+</html>
