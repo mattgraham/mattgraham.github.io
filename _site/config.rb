53f9bb49e2000acb538fbbad8a5a4cc761f5d261
Matt Graham <graham@github.com>
2013-10-30 11:43:55 -0400

    updating page structure

--- _site/config.rb
+++ _site/config.rb
@@ -1,8 +1,8 @@
 http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/scss"
-images_dir = "assets/images"
-javascripts_dir = "assets/js"
+css_dir = "/assets/css"
+sass_dir = "/assets/scss"
+images_dir = "/assets/images"
+javascripts_dir = "/assets/js"
 
 output_style = :compressed
 # relative_assets = true
